f9bb37a75e7bdc6cd7a305fcff6d2578b9b2552d
davidsquines <davidsquines@gmail.com>
2022-09-11 14:35:29 -0700

    Updates

---
+++ precache-manifest.15b424754bd562084f6681ac2615bc1a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9a42365b514753a994f",
+    "url": "/david.q/static/css/main.c6a8c0f6.chunk.css"
+  },
+  {
+    "revision": "c9a42365b514753a994f",
+    "url": "/david.q/static/js/main.c9a42365.chunk.js"
+  },
+  {
+    "revision": "a7eb0be1e1d3a71d6052",
+    "url": "/david.q/static/js/1.a7eb0be1.chunk.js"
+  },
+  {
+    "revision": "55562fb69f1758579274",
+    "url": "/david.q/static/js/runtime~main.55562fb6.js"
+  },
+  {
+    "revision": "69f0304b2cad5c1c2761bd6212574238",
+    "url": "/david.q/static/media/DavidQuinesResume9-6.69f0304b.pdf"
+  },
+  {
+    "revision": "a3a719858d8f453d5cd9b74870849566",
+    "url": "/david.q/static/media/profilePic.a3a71985.jpg"
+  },
+  {
+    "revision": "954b877abcbdc472fe3bb91c05966abe",
+    "url": "/david.q/static/media/gotgains.954b877a.mp4"
+  },
+  {
+    "revision": "3bb677f13590a5df31ca7dd49d438293",
+    "url": "/david.q/static/media/eureqavideo.3bb677f1.mp4"
+  },
+  {
+    "revision": "d635e7544bdaa1be1255cd0e7d50e7da",
+    "url": "/david.q/static/media/chorosX.d635e754.mp4"
+  },
+  {
+    "revision": "bbb3687b36deccc35790ac859801b19a",
+    "url": "/david.q/static/media/dictattack.bbb3687b.png"
+  },
+  {
+    "revision": "602284594ff3122c708bb813a2e195ea",
+    "url": "/david.q/static/media/comingsoon.60228459.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/david.q/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "4007cf06ceee1463c9129ac278f2b6e5",
+    "url": "/david.q/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/david.q/precache-manifest.15b424754bd562084f6681ac2615bc1a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/david.q/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
